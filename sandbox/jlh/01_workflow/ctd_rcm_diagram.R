f0f4f2b61f0598e49b9fd306985caa37921b9eac
Jaimie Harbin <jm810136@dal.ca>
2022-08-15 10:19:40 -0300

    updated diagram:

--- sandbox/jlh/01_workflow/ctd_rcm_diagram.R
+++ sandbox/jlh/01_workflow/ctd_rcm_diagram.R
@@ -12,15 +12,23 @@ grViz("digraph flowchart {
       tab6 [label = '@@6']
       tab7 [label = '@@7']
       tab8 [label = '@@8']
-      node [fontname = Helvetica, shape = rectangle, color=blue]
       tab9 [label = '@@9']
       tab10 [label = '@@10']
+      node [fontname = Helvetica, shape = rectangle, color=blue]
       tab11 [label = '@@11']
       tab12 [label = '@@12']
       tab13 [label = '@@13']
       tab14 [label = '@@14']
       tab15 [label = '@@15']
       tab16 [label = '@@16']
+      tab17 [label = '@@17']
+      tab18 [label = '@@18']
+      tab19 [label = '@@19']
+      tab20 [label = '@@20']
+
+
+
+
 
       # edge definitions with the node IDs
       tab1 -> tab2;
@@ -30,31 +38,39 @@ grViz("digraph flowchart {
       tab5 -> tab6;
       tab6 -> tab7;
       tab7 -> tab8;
+      tab8 -> tab9;
       tab9 -> tab10;
-      tab10 -> tab11;
       tab11 -> tab12;
       tab12 -> tab13;
       tab13 -> tab14;
       tab14 -> tab15;
       tab15 -> tab16;
+      tab16 -> tab17;
+      tab17 -> tab18;
+      tab18 -> tab19;
+      tab19 -> tab20
       }
 
-      [1]: 'Check ODF metadata'
-      [2]: 'Fix necessary changes'
-      [3]: 'Remove computed data'
-      [4]: 'Remove computed metadata'
-      [5]: 'Convert CR to conductivity'
-      [6]: 'Check range of conductivity'
-      [7]: 'Fix metadata'
-      [8]: 'Convert to NetCDF'
-      [9]: 'odfMetadataCheck()'
-      [10]: 'nameReplacement()'
-      [11]: 'ctdRemoveDerivedData()'
-      [12]: 'ctdRemoveDerivedMetadata()'
-      [13]: 'convertConductivityRatio()'
-      [14]: 'checkCrat()'
-      [15]: 'fixMetadata()'
-      [16]: 'convertNetCDF()'
+      [1]: 'Get necessary data'
+      [2]: 'Read ODF data'
+      [3]: 'Check ODF metadata'
+      [4]: 'Fix necessary changes'
+      [5]: 'Remove computed data'
+      [6]: 'Remove computed metadata'
+      [7]: 'Convert CR to conductivity'
+      [8]: 'Check range of conductivity'
+      [9]: 'Fix metadata'
+      [10]: 'Convert to NetCDF'
+      [11]: 'getData()'
+      [12]: 'read.oce()'
+      [13]: 'odfMetadataCheck()'
+      [14]: 'nameReplacement()'
+      [15]: 'ctdRemoveDerivedData()'
+      [16]: 'ctdRemoveDerivedMetadata()'
+      [17]: 'convertConductivityRatio()'
+      [18]: 'checkCrat()'
+      [19]: 'fixMetadata()'
+      [20]: 'convertNetCDF()'
       ")
 
 if (!interactive()) dev.off()

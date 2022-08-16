8913d4eae923108c95ac76d30ce7d477618a26c8
Jaimie Harbin <jm810136@dal.ca>
2022-08-16 10:45:32 -0300

    fix fixMetadata / add cratToConductivity

--- R/fixMetadata.R
+++ R/fixMetadata.R
@@ -4,32 +4,40 @@
 #' and adds place holders for flags if they do not already exist.
 #'
 #' @param odf an odf object (oce::read.odf())
-#'
+#' @param data a data frame of standard name, name, units, and GF3 codes likely from getData
 #' @param debug integer value indicating level of debugging.
 #'  If this is less than 1, no debugging is done. Otherwise,
 #'  some functions will print debugging information.
 #'
 #' @return an odf object with proper units and flags
 
-fixMetadata <- function(odf, debug=0) {
+fixMetadata <- function(odf, debug=0, data=NULL) {
+
+  if (is.null(data)) {
+    stop("must provide a dataframe data, likely from getData()")
+  }
+
+  if (!(class(data) == "data.frame")) {
+    stop("the data must be of class data.frame, not ", class(data))
+  }
 
   units <- odf@metadata$units
-  data <- names(odf@data)
-  l <- vector(mode="list", length=length(data))
-  names(l) <- data
+  DATA <- names(odf@data)
+  l <- vector(mode="list", length=length(DATA))
+  names(l) <- DATA
 
-  for (i in seq_along(data)) {
-    keep <- DF$units[which(DF$standard_name == gsub("_[0-9]$.*","",data[i])
+  for (i in seq_along(DATA)) {
+    keep <- data$units[which(data$standard_name == gsub("_[0-9]$.*","",DATA[i])
 )]
-    unit <- odf@metadata$units[data[i]][[1]][1]$unit
+    unit <- odf@metadata$units[DATA[i]][[1]][1]$unit
     if (debug > 0) {
-    message(keep, " is the units in dataframe for ", data[i], " and ", unit, " is what is in the odf")
+    message(keep, " is the units in dataframe for ", DATA[i], " and ", unit, " is what is in the odf")
     }
-    odf@metadata$units[data[i]][[1]][1]$unit <- keep
+    odf@metadata$units[DATA[i]][[1]][1]$unit <- keep
 
     ## Populating flags
     if (length(odf@metadata$flags) == 0) {
-    L<- rep(NA_real_, length(unlist(odf@data[data[i]])))
+    L<- rep(NA_real_, length(unlist(odf@data[DATA[i]])))
     l[[i]] <- L
     } else {
       message("Flags already exist for this profile.")

013f57545330ed350a02c9c40f57d8ba9d90b22e
Jaimie Harbin <jm810136@dal.ca>
2022-08-16 11:43:00 -0300

    update flow

--- sandbox/jlh/01_workflow/ctd_rcm_diagram.R
+++ sandbox/jlh/01_workflow/ctd_rcm_diagram.R
@@ -11,18 +11,14 @@ grViz("digraph flowchart {
       tab5 [label = '@@5']
       tab6 [label = '@@6']
       tab7 [label = '@@7']
+      node [fontname = Helvetica, shape = rectangle, color=blue]
       tab8 [label = '@@8']
       tab9 [label = '@@9']
-      node [fontname = Helvetica, shape = rectangle, color=blue]
       tab10 [label = '@@10']
       tab11 [label = '@@11']
       tab12 [label = '@@12']
       tab13 [label = '@@13']
       tab14 [label = '@@14']
-      tab15 [label = '@@15']
-      tab16 [label = '@@16']
-      tab17 [label = '@@17']
-      tab18 [label = '@@18']
 
       # edge definitions with the node IDs
       tab1 -> tab2;
@@ -31,16 +27,12 @@ grViz("digraph flowchart {
       tab4 -> tab5;
       tab5 -> tab6;
       tab6 -> tab7;
-      tab7 -> tab8;
       tab8 -> tab9;
+      tab9 -> tab10;
       tab10 -> tab11;
       tab11 -> tab12;
       tab12 -> tab13;
       tab13 -> tab14;
-      tab14 -> tab15;
-      tab15 -> tab16;
-      tab16 -> tab17;
-      tab17 -> tab18;
       }
 
       [1]: 'Get necessary data'
@@ -48,19 +40,15 @@ grViz("digraph flowchart {
       [3]: 'Check ODF metadata'
       [4]: 'Fix necessary changes'
       [5]: 'Remove derived data/metadata'
-      [6]: 'Convert CR to conductivity'
-      [7]: 'Check range of conductivity'
-      [8]: 'Fix metadata'
-      [9]: 'Convert to NetCDF'
-      [10]: 'getData()'
-      [11]: 'read.oce()'
-      [12]: 'odfMetadataCheck()'
-      [13]: 'nameReplacement()'
-      [14]: 'removeDerived()'
-      [15]: 'convertConductivityRatio()'
-      [16]: 'checkCrat()'
-      [17]: 'fixMetadata()'
-      [18]: 'convertNetCDF()'
+      [6]: 'Polish ODF'
+      [7]: 'Convert to NetCDF'
+      [8]: 'getData()'
+      [9]: 'read.oce()'
+      [10]: 'odfMetadataCheck()'
+      [11]: 'nameReplacement()'
+      [12]: 'removeDerived()'
+      [13]: 'polishODF()'
+      [14]: 'convertNetCDF()'
       ")
 
 if (!interactive()) dev.off()

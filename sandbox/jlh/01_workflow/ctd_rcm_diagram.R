b3f2d85a7d2ea296f50a90589331fb2d6d7977e3
Jaimie Harbin <jm810136@dal.ca>
2022-08-16 12:13:37 -0300

    update flow diagram

--- sandbox/jlh/01_workflow/ctd_rcm_diagram.R
+++ sandbox/jlh/01_workflow/ctd_rcm_diagram.R
@@ -10,15 +10,13 @@ grViz("digraph flowchart {
       tab4 [label = '@@4']
       tab5 [label = '@@5']
       tab6 [label = '@@6']
-      tab7 [label = '@@7']
       node [fontname = Helvetica, shape = rectangle, color=blue]
+      tab7 [label = '@@7']
       tab8 [label = '@@8']
       tab9 [label = '@@9']
       tab10 [label = '@@10']
       tab11 [label = '@@11']
       tab12 [label = '@@12']
-      tab13 [label = '@@13']
-      tab14 [label = '@@14']
 
       # edge definitions with the node IDs
       tab1 -> tab2;
@@ -26,29 +24,25 @@ grViz("digraph flowchart {
       tab3 -> tab4;
       tab4 -> tab5;
       tab5 -> tab6;
-      tab6 -> tab7;
+      tab7 -> tab8;
       tab8 -> tab9;
       tab9 -> tab10;
       tab10 -> tab11;
       tab11 -> tab12;
-      tab12 -> tab13;
-      tab13 -> tab14;
       }
 
       [1]: 'Get necessary data'
       [2]: 'Read ODF data'
-      [3]: 'Check ODF metadata'
-      [4]: 'Fix necessary changes'
-      [5]: 'Remove derived data/metadata'
-      [6]: 'Polish ODF'
-      [7]: 'Convert to NetCDF'
-      [8]: 'getData()'
-      [9]: 'read.oce()'
-      [10]: 'odfMetadataCheck()'
-      [11]: 'nameReplacement()'
-      [12]: 'removeDerived()'
-      [13]: 'polishODF()'
-      [14]: 'convertNetCDF()'
+      [3]: 'Fix necessary changes'
+      [4]: 'Remove derived data/metadata'
+      [5]: 'Polish ODF'
+      [6]: 'Convert to NetCDF'
+      [7]: 'getData()'
+      [8]: 'read.oce()'
+      [9]: 'nameReplacement()'
+      [10]: 'removeDerived()'
+      [11]: 'polishODF()'
+      [12]: 'convertNetCDF()'
       ")
 
 if (!interactive()) dev.off()

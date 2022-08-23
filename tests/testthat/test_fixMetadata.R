8f2b43538fab5a5c13a79681671cce27f0a6a7d4
Jaimie Harbin <jm810136@dal.ca>
2022-08-23 14:56:48 -0300

    fixMetadata test

---
+++ tests/testthat/test_fixMetadata.R
@@ -0,0 +1,32 @@
+## vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
+
+library(odfToNetCDF)
+
+context("fixMetadata")
+
+library(odfToNetCDF)
+library(oce)
+data <- getData(type="ctd")
+f <- system.file("extdata", "mctd.ODF", package="odfToNetCDF")
+odf1 <- read.odf(f)
+odf2 <- nameReplacement(odf1, data=data, unit="S/m")
+odf3 <- removeDerived(odf2)
+
+test_that("fixMetadata",
+          {
+            odf4 <- fixMetadata(odf3, data=data)
+            expect_equal(names(odf4[['metadata']]$flags[1]), "sea_water_temperature_1")
+            expect_equal(odf4[['metadata']]$units[[1]]$unit, "degree_C")
+
+
+          }
+)
+
+test_that("errors",
+          {
+            expect_error(fixMetadata(odf3), "must provide a dataframe data, likely from getData()")
+            expect_error(fixMetadata(odf1, data=1:10), "the data must be of class data.frame, not integer")
+
+
+          }
+)

9e2c5b5711b4bec517075c054fad1020a3cc781a
Jaimie Harbin <jm810136@dal.ca>
2022-09-06 10:46:32 -0300

    progress

--- sandbox/jlh/03_new_adcp_flow/adcp2.R
+++ sandbox/jlh/03_new_adcp_flow/adcp2.R
@@ -7,6 +7,6 @@ path <- "../davisStrait/data/mooring/2004-05/ADCP/BI3/"
 files <- list.files(path=path, pattern="*.ODF", ignore.case=TRUE, full.names = TRUE)
 adps <- lapply(files, read.oce)
 source("sandbox/jlh/03_new_adcp_flow/compile2.R"); adps3 <- compile2(files, debug=1)
-source("sandbox/jlh/03_new_adcp_flow/test.R"); testADP(adps3, data=data, name="TEST", debug=1)
+source("sandbox/jlh/03_new_adcp_flow/single2.R"); single2(adps3, data=data, name="TEST", debug=1)
 
 #singleAdpNetCDF(adps3, data=data, name="j", debug=1)

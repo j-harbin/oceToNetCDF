cc87142341f76c007b26a2c7863a52ebb79222d3
Jaimie Harbin <jm810136@dal.ca>
2023-03-16 09:10:46 -0300

    fix ctd work flow mistake

--- vignette/ctd_rcm_diagram.R
+++ vignette/ctd_rcm_diagram.R
@@ -27,44 +27,44 @@ grViz("digraph flowchart {
 
       tab1 -> tab2;
       tab2 -> tab3;
+      tab3 -> tab5;
+      tab5 -> tab11;
+      tab11 -> tab12;
+      tab12 -> tab14;
+      tab14 -> tab15;
+      tab15 -> tab16;
+      tab16 -> tab17;
+      tab17 -> tab18;
       tab2 -> tab4;
-      tab3 ->tab5;
-      tab4 ->tab6;
-      tab5 -> tab6;
+      tab4 -> tab6;
       tab6 -> tab7;
       tab7 -> tab9;
-      tab9 -> tab10;
-      tab10 -> tab11;
-      tab11 -> tab12;
-      tab12 -> tab13;
-      tab6 ->tab8;
-      tab8 -> tab14;
-      tab14 -> tab15;
-      tab15 -> tab17;
-      tab17 -> tab10;
-      tab14 -> tab16;
-      tab16 ->tab18;
+      tab9 -> tab11;
+      tab11 -> tab13;
+      tab13 -> tab15;
+      tab6 -> tab8;
+      tab8 -> tab10;
 
       }
 
       [1]: 'getStandardData()'
-      [2]: 'Do you want IOOS standards?'
+      [2]: 'Is file ODF origin?'
       [3]: 'Yes'
       [4]: 'No'
-      [5]: 'standardMetadata()'
-      [6]: 'Is the file origin an ODF?'
+      [5]: 'read.oce()'
+      [6]: 'Is file matlab origin?'
       [7]: 'Yes'
       [8]: 'No'
-      [9]: 'read.oce()'
-      [10]: 'nameReplacement()'
-      [11]: 'removeDerived()'
-      [12]: 'fixMetadata()'
-      [13]: 'convertNetCDF()'
-      [14]: 'Is the file origin matlab?'
-      [15]: 'Yes'
-      [16]: 'No'
-      [17]: 'matlabToOce()'
-      [18]: 'Unable to convert this file type.'
+      [9]: 'matlabToOce()'
+      [10]: 'Unable to convert this file type'
+      [11]: 'Do you want IOOS standards'
+      [12]: 'Yes'
+      [13]: 'No'
+      [14]: 'standardMetadata()'
+      [15]: 'nameReplacement()'
+      [16]: 'removeDerived()'
+      [17]: 'fixMetadata()'
+      [18]: 'convertNetCDF()'
       ")
 
 if (!interactive()) dev.off()
